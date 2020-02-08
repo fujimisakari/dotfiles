c62f91ec0bf1c98949553f39018044a4c0870c8b
Fujimisakari <fujimisakari@gmail.com>
2020-02-09 07:12:35 +0900

    update screen setting

--- init.py
+++ init.py
@@ -26,11 +26,10 @@ for dotfile in dotfiles_list:
 
 platform = platform.system()
 if platform == 'Darwin':
-    system('ln -sf ~/dotfiles/.screenrc_mac ~/.screenrc')
-    system('ln -sf ~/dotfiles/.screeninator_mac ~/.screeninator')
+    system('ln -sf ~/dotfiles/.screeninator_mac/default.screen ~/.screenrc')
     system('ln -sf ~/dotfiles/.swiftlint.yml ~/.')
 elif platform == 'Linux':
-    system('ln -sf ~/dotfiles/.screenrc_linux ~/.screenrc')
+    system('ln -sf ~/dotfiles/.screeninator_linux/default.screen ~/.screenrc')
     system('ln -sf ~/dotfiles/.screeninator_linux ~/.screeninator')
 
 system('touch $HOME/.screen-exchange')
