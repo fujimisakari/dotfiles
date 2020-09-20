b88cad25d54304b0159742a66807a4b05df9179d
Fujimisakari <fujimisakari@gmail.com>
2020-09-21 02:58:40 +0900

    update xset rate

--- .ubuntu-config/xkeysnail_bluetooth.sh
+++ .ubuntu-config/xkeysnail_bluetooth.sh
@@ -9,6 +9,7 @@ id=`sudo ~/.pyenv/shims/xkeysnail ~/.xkeysnailrc.py | grep "ThinkPad Compact Blu
 if [ -n "${id}" ]; then
   echo "load thinkpad Bluetooth Keyboard device: /dev/input/event${id}"
   sudo `which xkeysnail` ~/.xkeysnailrc.py --devices "/dev/input/event${id}" > /dev/null 2>&1 &
-  xset r rate 170 35
+  # xset r rate 170 35
+  xset r rate 200 30
   xinput --set-prop "pointer:ThinkPad Compact Bluetooth Keyboard with TrackPoint" "Coordinate Transformation Matrix" 1 0 0 0 1 0 0 0 2
 fi
